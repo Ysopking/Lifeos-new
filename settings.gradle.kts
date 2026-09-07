d901fe651e5ce6ad7533c490e795744427038420
Ysopking <mineflo1995@gmail.com>
2026-09-07 23:43:33 +0200

    Add language understanding core module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,4 +6,4 @@ dependencyResolutionManagement {
     repositories { google(); mavenCentral() }
 }
 rootProject.name = "LIFEOS-Next"
-include(":app", ":core:model", ":core:runtime", ":core:data", ":core:image", ":core:image-native")
+include(":app", ":core:model", ":core:runtime", ":core:data", ":core:image", ":core:image-native", ":core:language")
