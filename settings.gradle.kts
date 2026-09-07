cfc4b03170ee870b94c71cbdb071e25b114d8a1f
Ysopking <mineflo1995@gmail.com>
2026-09-07 13:11:30 +0200

    build: add settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+pluginManagement {
+    repositories { google(); mavenCentral(); gradlePluginPortal() }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories { google(); mavenCentral() }
+}
+rootProject.name = "LIFEOS-Next"
+include(":app", ":core:model", ":core:runtime")
