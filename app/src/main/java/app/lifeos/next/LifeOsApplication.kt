0e0c7dd54d3eea4dd57c0dd7644924c21cdd1892
Ysopking <mineflo1995@gmail.com>
2026-09-07 15:46:06 +0200

    Add LIFEOS application composition owner

---
+++ app/src/main/java/app/lifeos/next/LifeOsApplication.kt
@@ -0,0 +1,16 @@
+package app.lifeos.next
+
+import android.app.Application
+import app.lifeos.next.kernel.LifeOsKernel
+import app.lifeos.next.kernel.LifeOsKernelFactory
+
+/** Process-level owner for the LIFEOS kernel instance. */
+class LifeOsApplication : Application() {
+    lateinit var kernel: LifeOsKernel
+        private set
+
+    override fun onCreate() {
+        super.onCreate()
+        kernel = LifeOsKernelFactory(this).create()
+    }
+}
