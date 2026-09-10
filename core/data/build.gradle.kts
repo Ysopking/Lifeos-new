a20ac46f98ebbf82e3f046a1e4f59dcdafea762d
Ysopking <mineflo1995@gmail.com>
2026-09-10 10:17:26 +0200

    Wire data module to learning watermark contracts

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -9,6 +9,7 @@ android {
 dependencies {
     implementation(project(":core:model"))
     implementation(project(":core:field"))
+    implementation(project(":core:runtime"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     testImplementation(kotlin("test-junit"))
 }
