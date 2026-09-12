2563c1d5e6d2e753deb6440a6683c0b6e326cb72
Ysopking <mineflo1995@gmail.com>
2026-09-12 21:02:36 +0200

    Add BuildStudio JVM host module

---
+++ host/buildstudio/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins { id("org.jetbrains.kotlin.jvm") }
+
+kotlin { jvmToolchain(17) }
+
+dependencies {
+    implementation(project(":core:runtime"))
+    testImplementation(kotlin("test"))
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
+}
+
+tasks.test { useJUnitPlatform() }
