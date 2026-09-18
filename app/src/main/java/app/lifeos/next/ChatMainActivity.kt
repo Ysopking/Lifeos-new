223d0ec4918b01153a76483f97bb2fe6685a329f
Ysopking <mineflo1995@gmail.com>
2026-09-19 01:09:43 +0200

    F16: enable edge-to-edge LIFEOS presentation

--- app/src/main/java/app/lifeos/next/ChatMainActivity.kt
+++ app/src/main/java/app/lifeos/next/ChatMainActivity.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -82,6 +83,7 @@ class ChatMainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         val owner = application as LifeOsApplication
 
         setContent {
