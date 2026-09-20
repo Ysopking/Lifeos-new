6d448a62ea8df903a2398097434be54f41e40a6c
Ysopking <mineflo1995@gmail.com>
2026-09-20 17:12:00 +0200

    B302.1: depend on runtime-personal module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,6 +28,7 @@ android {
 dependencies {
     implementation(project(":core:model"))
     implementation(project(":core:runtime"))
+    implementation(project(":core:runtime-personal"))
     implementation(project(":core:data"))
     implementation(project(":core:image"))
     implementation(project(":core:image-native"))
